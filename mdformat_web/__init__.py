61a776e66ad4f7d6e00a471f1797abdb6fc92b2c
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-10-23 09:58:26 +0300

    Bump version: 0.0.0 → 0.0.1

--- mdformat_web/__init__.py
+++ mdformat_web/__init__.py
@@ -1,7 +1,7 @@
 import cssbeautifier
 import jsbeautifier
 
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.0.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 
 def format_js(unformatted: str, _info_str: str) -> str:
