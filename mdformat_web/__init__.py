e3297644b39ce997e2c6910d9fc7cf6fa0243a86
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2024-10-15 18:59:27 +0300

    Bump version: 0.1.0 → 0.2.0

--- mdformat_web/__init__.py
+++ mdformat_web/__init__.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import cssbeautifier
 import jsbeautifier
 
-__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.2.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 
 def format_js(unformatted: str, _info_str: str) -> str:
